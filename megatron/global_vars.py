c3773d5e9fd1d030e684b7787b103ef403dfa75f
Lucas Pickup <lupickup@microsoft.com>
2023-02-14 02:28:13 +0000

    Start tensorboard on last worker of node-0 not last worker

--- megatron/global_vars.py
+++ megatron/global_vars.py
@@ -141,7 +141,7 @@ def _set_tensorboard_writer(args):
                                    'tensorboard writer')
 
     if hasattr(args, 'tensorboard_dir') and \
-       args.tensorboard_dir and args.rank == (args.world_size - 1):
+       args.tensorboard_dir and args.rank == (torch.cuda.device_count() - 1): # last trainer on node-0
         try:
             from torch.utils.tensorboard import SummaryWriter
             print('> setting tensorboard ...')
